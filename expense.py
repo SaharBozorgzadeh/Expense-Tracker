8cdc0ca6e5b192276e3fa5f6ac5e0b18899699e7
Sahar <sahar.85bozorgzadeh@gmail.com>
2026-09-17 17:15:19 +0330

    "Add Expense class"

---
+++ expense.py
@@ -0,0 +1,8 @@
+class Expense:
+
+    def __init__(self, amount, category, description, date, id):
+        self.amount = amount
+        self.category = category
+        self.description = description
+        self.date = date
+        self.id = id
