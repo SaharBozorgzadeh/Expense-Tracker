442a2c7143bf2b459ef18ab008535dc7b7ef25ae
Sahar <sahar.85bozorgzadeh@gmail.com>
2026-09-17 17:58:43 +0330

    "Implement editExpenses function"

--- manageExpenses.py
+++ manageExpenses.py
@@ -42,11 +42,63 @@ def addExpenses():
         menu()
         break
 
-def viewExpenses() :
+
+def viewExpenses():
     if not expensesList:
         print("No expenses found.")
-    else :
-       print(f"{'ID':<5}{'Date':<15}{'Category':<15}{'Amount':<12}{'Description'}")
-    for expense in expensesList:
-        print(f"{expense.id:<5}{expense.date:<15}{expense.category:<15}{expense.amount:<12}{expense.description}")
+    else:
+        print(f"{'ID':<5}{'Date':<15}{'Category':<15}{'Amount':<12}{'Description'}")
+        for expense in expensesList:
+            print(
+                f"{expense.id:<5}{expense.date:<15}{expense.category:<15}{expense.amount:<12}{expense.description}"
+            )
     menu()
+    return
+
+
+def editExpenses():
+    # Find the expense by ID
+    expense_id = int(input("Please enter expense ID: "))
+    expense = next((i for i in expensesList if i.id == expense_id), None)
+    if expense is None:
+        print("There is no such expense")
+        menu()
+        return
+
+    # Change the amount
+    print(f"Current Amount: {expense.amount}")
+    try:
+        new_amount = float(input("New amount: "))
+
+        if new_amount <= 0:
+            print("Amount must be bigger than 0, editing failed")
+            menu()
+            return
+        expense.amount = new_amount
+    except ValueError:
+        print("Amount must be a number!, editing failed")
+        menu()
+        return
+    
+
+    # Change the category
+    print(f"Current category: {expense.category}")
+    new_category = input("New category: ")
+
+    if not new_category.strip():
+        print("Category cannot be empty.")
+        menu()
+        return
+    expense.category = new_category
+
+    # Change the description
+    print(f"Current description: {expense.description}")
+    new_description = input("Description: ")
+    expense.description = new_description
+
+    # Change the date
+    print(f"Current date: {expense.date}")
+    new_date = input("Date: ")
+    if not new_date.strip():
+        new_date = datetime.date.today().isoformat()
+    expense.date = new_date
